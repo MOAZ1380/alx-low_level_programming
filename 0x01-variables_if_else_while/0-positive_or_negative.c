339a4439be6630a6c6e7f32a41209adaa1c7816b
moaz <moaz469654@gmail.com>
2023-09-08 08:46:56 -0700

    moaz

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,22 +10,22 @@
 */
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-if (n > 0)
-{
-printf("%d is positive", n);
-}
-else if (n == 0)
-{
-printf("%d is zero", n);
-}
-eldsse
-{
-printf("%d negative", n);
-}
+	if (n > 0)
+	{
+	printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero", n);
+	}
+	else
+	{
+	printf("%d negative", n);
+	}
 	return (0);
 }
