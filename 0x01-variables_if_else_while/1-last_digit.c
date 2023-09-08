f71101f030c2dc42e186f47b0e617caa10fc9737
moaz <moaz469654@gmail.com>
2023-09-08 11:30:13 -0700

    moaz

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ n = rand() - RAND_MAX / 2;
 digit = n % 10;
 if (digit > 5)
 {
-printf("Last digit of %i is %i and is greater than 5\n", n, digital);
+printf("Last digit of %i is %i and is greater than 5\n", n, digit);
 }
 else if (digit == 0)
 {
