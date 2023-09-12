918f89c83f70806664e374fd7005d4cca04e4639
moaz <moaz469654@gmail.com>
2023-09-12 08:29:01 -0700

    two

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_alphabet - utilizes on the _putchar function to print
+*/
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+		printf("%d", c);
+
+		printf("\n");
+	}
+		return (0);
+}
