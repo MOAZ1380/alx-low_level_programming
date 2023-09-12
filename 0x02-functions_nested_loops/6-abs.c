9f7ecc765d3b699b7e9937c77f4a7b7f29005065
moaz <moaz469654@gmail.com>
2023-09-12 14:49:39 -0700

    seven

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _abs- function that computes
+ *
+ * @n: takes in integer type input for function
+ *
+ * Return: 0 Always
+*/
+int _abs(int)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
