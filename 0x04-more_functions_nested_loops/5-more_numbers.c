651f2c61edd45c041054ce9b160d0fa5e6282a80
moaz <moaz469654@gmail.com>
2023-09-14 16:33:15 -0700

    moaz

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * more_numbers - check
+ *
+ * Return: Always 0.
+*/
+void more_numbers(void)
+{
+	int x, n;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			printf("%d", n);
+		}
+		printf("\n");
+	}
+}
