0775e0e13c59386cc5d61a7b0704c4016dae6020
moaz <moaz469654@gmail.com>
2023-09-14 16:59:43 -0700

    moaz

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * print_diagonal - check
+ *
+ * @n: is the
+ *
+ * Return: Always 0.
+*/
+void print_diagonal(int n)
+{
+	int po, sp;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (po = 1; po <= n; po++)
+		{
+			for (sp = 1; sp <= po; sp++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
