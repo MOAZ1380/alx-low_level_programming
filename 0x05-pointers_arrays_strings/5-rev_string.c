95192f44e5a8c1f9fae4f3ce2b1a75482d024c40
moaz <moaz469654@gmail.com>
2023-09-18 17:46:01 -0700

    moaz

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,13 @@ void rev_string(char *s)
 {
 	int l, i;
 	char temp;
+	/*find stringfdsjkdkj fdskjfksdj*/
 		for (l = 0; s[l] != '\0'; ++l)
+			/*swap string flsdklfdsjfdslkfkds char*/
 		for (i = 0; i < l / 2; i++)
 	{
 			temp = s[i];
-		s[i] = s[l - 1 - i];
+		s[i] = s[l - 1 - i];/* 1 because dfflkjfdsdfifdsijio 0*/
 	s[l - 1 - i] = temp;
 	}
 }
