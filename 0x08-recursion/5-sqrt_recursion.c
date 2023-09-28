eb3341f0e7ab9dd7f203eec1aa827fbd6b1f7954
moaz <moaz469654@gmail.com>
2023-09-28 16:47:34 -0700

    moaz

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include"main.h"
+#include"stdio.h"
+int _sqrt(int n, int i);
+
+/**
+ * _sqrt - moaz
+ * @n: mo
+ * Return: moaz
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - moa
+ * @n: moa
+ * @i: moaz
+ *
+ * Return: mo
+*/
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
+	}
