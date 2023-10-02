b6207550d707d47f2f28bc8761946ed02003e784
moaz <moaz469654@gmail.com>
2023-10-02 10:21:54 -0700

    moaz

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+/**
+ * main - moaz
+ * @argc: m
+ * @argv: m
+ * Return: mo
+*/
+int main(int argc, char *argv[])
+{
+	int n1 = 0;
+	int n2 = 0;
+		if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%i\n", n1 * n2);
+			return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
