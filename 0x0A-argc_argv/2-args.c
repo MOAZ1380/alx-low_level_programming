9eb550749e1f65e20df7924cec00daaaaccfd4c2
moaz <moaz469654@gmail.com>
2023-10-02 10:05:33 -0700

    moaz

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - mo
+ * @argc: mo
+ * @argv: moa
+ * Return: mo
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+		for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
