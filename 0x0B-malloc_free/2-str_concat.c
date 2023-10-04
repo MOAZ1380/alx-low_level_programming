bb6d3542a7cc70f06f868c86d1aeab450b983f75
moaz <moaz469654@gmail.com>
2023-10-04 05:23:00 -0700

    moaz

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,20 +7,39 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	char *i;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+	while (s1[i])
+		i++;
 
-	i = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
+	while (s2[j])
+		j++;
 
-		if (i == NULL)
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+		if (s == NULL)
 			return (NULL);
-
-	i = strcat(s1, s2);
-		return (i);
+		j = 0;
+		while (k < l)
+		{
+			if (k <= i)
+			{
+				s[k] = s1[k];
+			}
+			if (k >= i)
+			{
+				s[k] = s2[j];
+				j++;
+			}
+			k++;
+		}
+	s[k] = '\0';
+	return (s);
 }
-
