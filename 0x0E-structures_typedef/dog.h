20fc43021a98e190a0c17640f48aa5c09d98b154
moaz <moaz469654@gmail.com>
2023-10-11 08:04:50 -0700

    moaz

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - m
+ * @name: m
+ * @age: m
+ * @owner: mo
+ * Description: mo
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
