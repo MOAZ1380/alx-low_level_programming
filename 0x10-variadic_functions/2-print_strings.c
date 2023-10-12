9682f78b681c3691cc065a4d6e14a868b9d21adb
moaz <moaz469654@gmail.com>
2023-10-12 06:46:29 -0700

    moaz

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,23 +1,24 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * print_numbers - m
+ * print_strings - m
  * @separator: moa
  * @n: m
  * @...: m
 */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list string;
 	char *str;
 	unsigned int index;
 
 	va_start(string, n);
-
 	for (index = 0; index < n; index++)
 	{
 		str = va_arg(string, char *);
-		if (str == NULL)
-			printf("(nill)");
+			if (str == NULL)
+			printf("(nil)");
 		else
 			printf("%s", str);
 			if (index != (n - 1) && separator != NULL)
