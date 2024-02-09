417015540833b1a6a59f961afb175b2fd6101c73
moaz <moaz469654@gmail.com>
2024-02-09 02:20:01 -0800

    moaz

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,21 +3,21 @@
 /**
  *  binary_to_uint - mo
  *  @b: mo
- *  return: mo
+ *  Return: mo
 */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	if (b == NULL)
-	return 0;
-	
-	while (*b != '\0')
-	{
-		if (*b != '0' && *b != '1')
-			return 0;
+if (b == NULL)
+	return (0);
 
-		result =+(*b - '0') + result * 2;
+while (*b)
+	{
+if (*b != '0' && *b != '1')
+	return (0);
+
+		result = (result * 2) + (*b - '0');
 		b++;
 	}
-	return result;
+	return (result);
 }
