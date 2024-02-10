dd8d3fca9f2fd5a079b4640710134d450bdd52dc
moaz <moaz469654@gmail.com>
2024-02-10 15:16:21 -0800

    moaz

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/***/
+char *string_toupper(char *str) {
+    char *ptr = str;
+    while (*ptr != '\0') {
+        if (*ptr >= 'a' && *ptr <= 'z') {
+            *ptr = *ptr - ('a' - 'A');
+        }
+        ptr++;
+    }
+    return str;
+}
