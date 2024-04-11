46a2ebd3f87994ea73148f9d41ebd91b7408412a
moaz <moaz469654@gmail.com>
2024-04-11 13:45:58 -0700

    moa

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,11 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int index;
 
-	new_table = malloc(sizeof(hash_table_t));
+	new_table =(hash_table_t *) malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	new_table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
